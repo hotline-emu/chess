1ce8a3914910af4dd8c7213855868901e4f30e54
redacted <redacted>
2025-05-24 09:51:32 -0400

    coverage plus plus

---
+++ tests/src/chess/components/test_piece_factory.py
@@ -0,0 +1,22 @@
+import pytest
+from chess.components import PieceFactory
+from chess.components.pieces.bishop import Bishop
+from chess.exceptions import PieceNotFoundError
+
+
+def test_create() -> None:
+    kind = Bishop.lookup_name
+    color = Bishop.BLACK
+
+    actual = PieceFactory.create(kind, color)
+    expected = Bishop(color)
+
+    assert expected.lookup_name == actual.lookup_name
+
+
+def test_create_not_found() -> None:
+    kind = "foo"
+    color = "bar"
+
+    with pytest.raises(PieceNotFoundError):
+        _ = PieceFactory.create(kind, color)
