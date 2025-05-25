282ae7a81580e9d19d74d3e0192560dda96d8171
Hotline Emu <11667661+hotline-emu@users.noreply.github.com>
2025-05-25 18:52:31 -0400

    overflow logic

--- tests/integration/test_problem_scenario.py
+++ tests/integration/test_problem_scenario.py
@@ -25,21 +25,33 @@ def test_the_problem(engine: Engine) -> None:
     die_a = Die()
     die_b = Die()
 
+    rook_initial_location = (7, 7)  # The bishop starts here per the scenario terms.
+    bishop_terminal_location = (5, 2)  # The bishop does not move.
+
+    rook_current_location = rook_initial_location
     rook_has_been_executed = False
     for turn_index in range(turns):
         nth_turn = turn_index + 1
         logger.info("turn %s", nth_turn)
         face, dice_value = __get_coin_and_dice_values(coin, die_a, die_b)
 
-        rook_current_location = (0, 1)
         rook_destination_location = __get_rook_desination_location(
             face,
             dice_value,
             rook_current_location,
         )
-        logger.info("the rook has moved to %s", rook_destination_location)
 
-        rook_has_been_executed = __can_rook_be_executed(rook_destination_location)
+        logger.info(
+            "rook moving from %s to %s",
+            rook_current_location,
+            rook_destination_location,
+        )
+        rook_current_location = rook_destination_location  #! TODO: Trigger board move.
+
+        rook_has_been_executed = __can_rook_be_executed(
+            rook_current_location,
+            bishop_terminal_location,
+        )
         if rook_has_been_executed:
             break
 
@@ -71,22 +83,44 @@ def __get_rook_desination_location(
     dice_value: int,
     current_location: tuple[int, int],
 ) -> tuple[int, int]:
-    destination_location = (1, 1)
+    current_rank, current_file = current_location
 
     if face == HEADS:
         logger.info("the rook is moving right %s spaces", dice_value)
         # rook moves up <dice_value> spaces
         # if rook exceeds the board top, they show back up on the bottom.
-        ...
+        # rightmost space is index 7 -> back to 0
+        rightmost_file_index = 7
+        target_file_index = current_file + dice_value
+        while target_file_index > rightmost_file_index:
+            logger.info("rook went off the board, moving to the first file")
+            target_file_index -= 8
 
-    if face == TAILS:
-        logger.info("the rook is moving up %s spaces", dice_value)
-        # rook moves right <dice_value> spaces
-        # if rook exceeds the right edge, they show back up on the left edge.
-        ...
+        destination_rank = current_rank
+        destination_file = target_file_index
+        return (
+            destination_rank,
+            destination_file,
+        )
 
-    return destination_location
+    # Face must logically be tails here.
+    logger.info("the rook is moving up %s spaces", dice_value)
+    # rook moves right <dice_value> spaces
+    # if rook exceeds the right edge, they show back up on the left edge.
+    # topmost space is index 0 -> back to 7
+    topmost_rank_index = 0
+    target_rank_index = current_rank - dice_value
+    while target_rank_index < topmost_rank_index:
+        logger.info("rook went off the board, moving to the first rank")
+        target_rank_index += 8
+
+    destination_rank = target_rank_index
+    destination_file = current_file
+    return (
+        destination_rank,
+        destination_file,
+    )
 
 
-def __can_rook_be_executed(rook_destination_location) -> bool:
+def __can_rook_be_executed(rook_current_location, bishop_current_location) -> bool:
     return False
--- todo.md
+++ todo.md
@@ -13,10 +13,14 @@ The Problem
 - Roll 2 dice (6 sided) - sum is how many spaces moved.
   - If rook overflows right, they appear on the same row, leftmost column.
   - If the rook overflows top, they appear on the same column, bottommost row.
-
 - If the rook survives 15 rounds it wins.
   - Else the bishop wins.
 - After each turn print the following:
   - coin toss result
   - dice results and sum
   - rooks position after moving
+
+## REMAINDER
+
+- find out where the rook is moving and move them.
+- find out of the bishop can execute the rook.
