d8cf6a5664c299ce34cbae9caac590e6f61edeed
Hotline Emu <11667661+hotline-emu@users.noreply.github.com>
2025-05-25 22:29:07 -0400

    full coverage

--- tests/unit/src/chess/game/scenario/test_scenarios.py
+++ tests/unit/src/chess/game/scenario/test_scenarios.py
@@ -1,8 +1,15 @@
-from chess.game.scenario.scenarios import setup_scenario
+from chess.game.scenario.scenarios import setup_scenario, problem_scenario
+
+
+def test_the_setup() -> None:
+    scenario = setup_scenario
+    actual = scenario()
+
+    assert isinstance(actual, list)
 
 
 def test_the_problem() -> None:
-    scenario = setup_scenario
+    scenario = problem_scenario
     actual = scenario()
 
     assert isinstance(actual, list)
